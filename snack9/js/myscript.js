55c4aaf42526236b4affccc66d8f7d384dfa183a
Sabelius <sbebby92@virgilio.it>
2021-08-27 17:05:16 +0200

    aggiornamenti finali

--- snack9/js/myscript.js
+++ snack9/js/myscript.js
@@ -1,12 +1,25 @@
 var firstList = ["0", "1", "2", "3", "4", "5"];
-var secondList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-var newList = ["0", "1", "2", "3", "4", "5"];
+var secondList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var longArr;
+var shortArr;
 
-for(var i = 0; i < 4; i++){ 
-  newList.push(Math.floor(Math.random() * secondList.length));
+if( firstList.length < secondList.length){
+  shortArr = firstList;
+  longArr = secondList;
+} else{
+  shortArr = secondList;
+  longArr = firstList;
+}
+
+var difference = longArr.length - shortArr.length;
+
+
+for( var i = 0; i < difference; i++){
+  shortArr.push(Math.floor(Math.random() * 10));
 }
 
 
-document.getElementById("first").innerHTML = firstList;
-document.getElementById("second").innerHTML = newList;
+document.getElementById("first").innerHTML = shortArr;
+document.getElementById("second").innerHTML = secondList;
+
 
